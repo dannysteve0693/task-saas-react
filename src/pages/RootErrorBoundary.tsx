37d437f896d7e10760f0ddd6e28211a0e01e83a4
Danny <dannysteve0693@gmail.com>
2025-06-07 10:02:35 +0700

    Completion of 404 Page

--- src/pages/RootErrorBoundary.tsx
+++ src/pages/RootErrorBoundary.tsx
@@ -6,29 +6,47 @@ import { Button } from '@/components/ui/button';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
+import { pageNotFound } from '@/assets';
+
 const RootErrorBoundary = () => {
   const error = useRouteError();
   return (
     <div className='min-h-[100dvh] flex flex-col'>
       <Header />
-      <div className='grow container'>
-        <h1 className=''>
+      <div className='grow container flex flex-col justify-center items-center pt-32 pb-12'>
+        <h1 className='text-2xl font-semibold text-center sm:text-4xl'>
           {isRouteErrorResponse(error)
             ? 'Hmmm, that page doesn’t exist.'
             : 'Something went wrong'}
         </h1>
 
-        <p className=''>
+        <p className='text-muted-foreground max-w-[55ch] text-center mt-4 mb-6 sm:text-lg'>
           {isRouteErrorResponse(error)
             ? 'You can get back on track and manage your tasks with ease.'
             : 'We`re working on fixing this issue. Please try again later.'}
         </p>
 
-        <div className=''>
+        <div className='flex gap-2'>
           <Button asChild>
             <Link to={'/'}>Return to Home</Link>
           </Button>
+
+          <Button
+            asChild
+            variant={'ghost'}
+          >
+            <Link to={'/app/inbox'}>View Inbox</Link>
+          </Button>
         </div>
+
+        <figure className='mt-10'>
+          <img
+            src={pageNotFound}
+            alt='404 page not found'
+            width={560}
+            height={373}
+          />
+        </figure>
       </div>
       <Footer />
     </div>
